548b28adff9d2f4eb988a00e7befa0847c9b32fc
descuret <Titouan.Descure@etu.univ-grenoble-alpes.fr>
2024-01-09 10:44:37 +0100

    bou bou bou gabin 2024

--- map/game1.js
+++ map/game1.js
@@ -23,12 +23,12 @@ class game1 extends Phaser.Scene {
         this.add.image(400, 300, 'sea');
 
         //création du joueur et de ces hitbox
-        this.player = this.physics.add.sprite(400, 300, 'player').setSize(90,90);
+        this.player = this.physics.add.sprite(400, 300, 'player').setSize(80,80);
         this.proue = this.physics.add.image();
-        this.proue.body.setCircle(10);
+        this.proue.body.setCircle(15);
 
         //création des énemies
-        this.enemie = this.physics.add.sprite(200, 200, 'enemie');
+        this.enemie = this.physics.add.sprite(200, 200, 'enemie').setSize(40,110);
 
         //attribution de la fonction a executer lors d'une collison avec un enemie
         this.physics.add.collider(this.proue,this.enemie, this.attack, null , this);
@@ -49,21 +49,78 @@ class game1 extends Phaser.Scene {
             this.isClicking = true;
         }
 
-        this.player.setAngularVelocity(50);
-        //this.proue.position.set(x - a.halfWidth, y - a.halfHeight)
-        //this.centerBodyOnXY(this.proue, this.player.body.x, this.player.body.y);
+        var dy = Math.abs(this.player.y - this.player.getData("positionY"));
+        var dx = Math.abs(this.player.x - this.player.getData("positionX"));
+
+
+
+        var vx = dx / dy;
+        var vy = dy / dx;
+
+
+
+        if (vx > 1) {
+            vx = 1;
+        }
+        if (vy > 1) {
+            vy = 1;
+        }
+
+        if(dx <= this.minDistance) {
+            this.player.setVelocityX(0);  
+        } else if(this.player.x < this.player.getData("positionX")) {
+            this.player.setVelocityX(400 * vx); 
+        } else if(this.player.x > this.player.getData("positionX")) {
+        this.player.setVelocityX(-400 * vx);
+        }
+
+        if(dy <= this.minDistance) {
+            this.player.setVelocityY(0);  
+            
+            
+        } else if(this.player.y < this.player.getData("positionY")) {
+            this.player.setVelocityY(400 * vy);
+            
+        } else if(this.player.y > this.player.getData("positionY")) {
+        this.player.setVelocityY(-400 * vy);
+        
+        }
+
+        if (dx <= this.minDistance && dy <= this.minDistance) {
+            this.canTurn = false;
+        }
+        
+        if (this.canTurn) {
+            var angle = Phaser.Math.RAD_TO_DEG * Phaser.Math.Angle.Between(this.player.x, this.player.y, this.player.getData("positionX"),  this.player.getData("positionY"));
+            this.player.setAngle(angle + 90);
+            this.canTurn = false;
+        }
 
         
+        
+        //this.player.setAngularVelocity(50);
+        
+        //place the proue hitbox
+        this.centerBodyOnXY(this.proue, this.player.body.x + 40, this.player.body.y - 18);
+        this.proue.body.updateCenter();
+        Phaser.Math.RotateAround(this.proue.body.center, this.player.body.center.x, this.player.body.center.y, this.player.rotation);
+        this.centerBodyOnPoint(this.proue, this.proue.body.center);
+
+        //add velocity to the proue for collision
+        this.proue.body.velocity.copy(this.player.body.velocity);
     }
 
     //centre l'object a par rapport a x,y
     centerBodyOnXY (object, x, y) {
-        console.log(object);
-        object.position.set(
-            x - object.halfWidth,
-            y - object.halfHeight
+        object.body.position.set(
+            x - object.body.halfWidth,
+            y - object.body.halfHeight
         );
     }
+
+    centerBodyOnPoint (a, p) {
+        this.centerBodyOnXY(a, p.x, p.y);
+      }
 
     
 
